93b75d2428dfb5a6ef4674e33ed2a035a096224d
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-02-26 11:21:09 +0100

    feat: ✨ refactor patient management and improve data fetching
    - Moved the Paziente interface to a new model file for better organization.
    - Updated the ListaPz component to use the PatientManager for patient data retrieval.
    - Removed hardcoded patient data and replaced it with dynamic data fetched from the API.
    - Enhanced the StatoAPI component to correctly reference the system status.

---
+++ his-afp/src/app/core/Pazienti/patient-manager.ts
@@ -0,0 +1,55 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { Paziente, PazienteDTO } from './Pazienti.model';
+import { HttpClient } from '@angular/common/http';
+import { APIResponse } from '../models/APIResponse.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PatientManager {
+  #http = inject(HttpClient);
+  #listaPZ = signal<Paziente[]>([]);
+  listaPZ = this.#listaPZ.asReadonly();
+
+  constructor() {
+    this.fetchPazienti();
+  }
+
+  public fetchPazienti() {
+    this.#http.get<APIResponse<PazienteDTO[]>>('http://localhost:3000/admissions').subscribe({
+      next: (res) => {
+        const pz = res.data.map((p) => this.mapPazienteDTOToPaziente(p));
+        this.#listaPZ.set(pz);
+      },
+      error: (err) => {
+        console.error('Errore durante il fetch dei pazienti:', err);
+      },
+    });
+  }
+
+  public mapPazienteDTOToPaziente(pz: PazienteDTO): Paziente {
+    return {
+      id: pz.id.toString(),
+      nome: pz.nome,
+      cognome: pz.cognome,
+      braccialetto: pz.braccialetto,
+      codiceColore: pz.coloreCode,
+      note: pz.noteTriage,
+      patologia: pz.patologiaCode,
+      eta: this.calcolaEta(pz.dataNascita),
+    };
+  }
+
+  public calcolaEta(dataNascita: string): number {
+    const today = new Date();
+    const birthDate = new Date(dataNascita);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+
+    return age;
+  }
+}
--- his-afp/src/app/lista-pz/lista-pz.ts
+++ his-afp/src/app/lista-pz/lista-pz.ts
@@ -1,11 +1,13 @@
-import { Component, computed, inject, model, signal } from '@angular/core';
-import { CardPz, Paziente } from '../card-pz/card-pz';
+import { Component, computed, inject, model } from '@angular/core';
+import { CardPz } from '../card-pz/card-pz';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { TagModule } from 'primeng/tag';
 import { SystemStatus } from '../core/SystemStatus/system-status';
 import { StatoAPI } from '../ui/stato-api/stato-api';
+import { Paziente } from '../core/Pazienti/Pazienti.model';
+import { PatientManager } from '../core/Pazienti/patient-manager';
 
 interface Response<T> {
   status: string;
@@ -19,59 +21,9 @@ interface Response<T> {
   styleUrl: './lista-pz.scss',
 })
 export class ListaPz {
+  readonly PatientManager = inject(PatientManager);
   nomePaziente = model<string>('');
-  listaPz = signal<Paziente[]>([
-    {
-      braccialetto: 'PR234',
-      codiceColore: 'VERDE',
-      cognome: 'Rocchio',
-      eta: 25,
-      id: '23',
-      nome: 'Pietro',
-      note: 'TRauma',
-      patologia: 'C19',
-    },
-    {
-      braccialetto: 'BR0034',
-      codiceColore: 'ARANCIONE',
-      cognome: 'Brazorf',
-      eta: 25,
-      id: '1',
-      nome: 'Ajeje',
-      note: 'TRauma',
-      patologia: 'C19',
-    },
-    {
-      braccialetto: 'LSD',
-      codiceColore: 'ROSSO',
-      cognome: 'Winky',
-      eta: 25,
-      id: '1',
-      nome: 'Tinky',
-      note: 'TRauma',
-      patologia: 'C19',
-    },
-    {
-      braccialetto: 'LSD',
-      codiceColore: 'AZZURRO',
-      cognome: 'Winky',
-      eta: 25,
-      id: '1',
-      nome: 'Tinky',
-      note: 'TRauma',
-      patologia: 'C19',
-    },
-    {
-      braccialetto: 'LSD',
-      codiceColore: 'AZZURRO',
-      cognome: 'Winky',
-      eta: 25,
-      id: '1',
-      nome: 'Tinky',
-      note: 'TRauma',
-      patologia: 'C19',
-    },
-  ]);
+  listaPz = this.PatientManager.listaPZ;
 
   healthStatus = inject(SystemStatus).statoAPI;
 
