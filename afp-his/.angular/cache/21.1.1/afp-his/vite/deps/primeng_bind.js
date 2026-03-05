20d8afb2e4c1106af8397f6b506fc60beeda9e51
Valerio <valerio.gravili@email.com>
2026-03-05 11:35:02 +0100

    fetched

---
+++ afp-his/.angular/cache/21.1.1/afp-his/vite/deps/primeng_bind.js
@@ -0,0 +1,11 @@
+import {
+  Bind,
+  BindModule
+} from "./chunk-SVUPFO7Z.js";
+import "./chunk-3PYSYKGX.js";
+import "./chunk-2ISLE7OS.js";
+import "./chunk-GOMI4DH3.js";
+export {
+  Bind,
+  BindModule
+};
---
+++ afp-his/.angular/cache/21.1.1/afp-his/vite/deps/primeng_config.js
@@ -0,0 +1,18 @@
+import {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+} from "./chunk-43OYTH4T.js";
+import "./chunk-34ZYDAY7.js";
+import "./chunk-IF6C2VPM.js";
+import "./chunk-GJUXJCMM.js";
+import "./chunk-3PYSYKGX.js";
+import "./chunk-2ISLE7OS.js";
+import "./chunk-GOMI4DH3.js";
+export {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+};
